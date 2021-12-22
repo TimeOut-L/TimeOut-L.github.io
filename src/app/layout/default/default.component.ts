dba8082f9435da60bf5a37deb0edbc759446b50a
TimeOut-L <760996145@qq.com>
2021-12-22 23:54:57 +0800

    3 files added

---
+++ src/app/layout/default/default.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector:"default-layout",
+  templateUrl:"./default.component.html",
+  styleUrls:["./default.componet.less"]
+})
+export  class DefaultLayoutComponent{}
