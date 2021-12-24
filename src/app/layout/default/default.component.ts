e9f507f4b277637c9fc1e73848e5b97cefc61893
TwilightTime <46495900+TimeOut-L@users.noreply.github.com>
2021-12-24 13:26:42 +0800

    Delete src directory

--- src/app/layout/default/default.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector:"default-layout",
-  templateUrl:"./default.component.html",
-  styleUrls:["./default.componet.less"]
-})
-export  class DefaultLayoutComponent{}
