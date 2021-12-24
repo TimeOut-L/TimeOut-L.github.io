b8dd95c00c7857c0d4b4d6d270ed3870ebcc180f
TimeOut-L <760996145@qq.com>
2021-12-24 14:46:46 +0800

    2 files modified, 3 added

--- src/app/layout/default/default.component.ts
+++ src/app/layout/default/default.component.ts
@@ -1,8 +1,10 @@
 import { Component } from "@angular/core";
+import { MenuComponent } from "./menu/menu.component";
 
 @Component({
   selector:"default-layout",
   templateUrl:"./default.component.html",
-  styleUrls:["./default.componet.less"]
+  styleUrls:["./default.componet.less"]，
+  declareL
 })
 export  class DefaultLayoutComponent{}
